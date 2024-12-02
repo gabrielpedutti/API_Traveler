ad53a82f440708f167a47f1b9af15ca10488cde8
gabrielpedutti <gabrielpedutti@hotmail.com>
2024-12-02 10:13:03 -0300

    Acrescentado mais retornos no getAllViagensPorUsuario

--- src/modules/viagem/useCases/getAllViagemsPorUsuario/GetAllViagemsPorUsuarioUseCase.ts
+++ src/modules/viagem/useCases/getAllViagemsPorUsuario/GetAllViagemsPorUsuarioUseCase.ts
@@ -6,10 +6,62 @@ export class GetAllViagemsPorUsuarioUseCase {
   async execute(data:GetViagemDTO): Promise<Viagem[]> {
     const viagems = await prisma.viagem.findMany({
       where: {
-        usuario_id: data.id, // Filtro pelo ID do usuário na tabela `viagem`
-      }
+        usuario_id: data.id,
+      },
+      include: {
+        viagem_origem: {
+          select: {
+            nm_municipio: true,
+            estado: {
+              select: {
+                nm_estado: true,
+                pais: {
+                  select: {
+                    nm_pais: true,
+                  },
+                },
+              },
+            },
+          },
+        },
+        viagem_destino: {
+          select: {
+            nm_municipio: true,
+            estado: {
+              select: {
+                nm_estado: true,
+                pais: {
+                  select: {
+                    nm_pais: true,
+                  },
+                },
+              },
+            },
+          },
+        },
+        status_viagem: {
+          select: {
+            descricao: true,
+          },
+        },
+      },
     });
+
+    // Corrigir o aninhamento dos resultados
+    const resultadoAplainado = viagems.map((viagem) => ({
+      ...viagem,
+      viagem_origem: {
+        nm_municipio: viagem.viagem_origem?.nm_municipio,
+        nm_estado: viagem.viagem_origem?.estado?.nm_estado,
+        nm_pais: viagem.viagem_origem?.estado?.pais?.nm_pais,
+      },
+      viagem_destino: {
+        nm_municipio: viagem.viagem_destino?.nm_municipio,
+        nm_estado: viagem.viagem_destino?.estado?.nm_estado,
+        nm_pais: viagem.viagem_destino?.estado?.pais?.nm_pais,
+      },
+    }));
     
-    return viagems;
+    return resultadoAplainado;
   }
 }
